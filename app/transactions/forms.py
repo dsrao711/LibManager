a3f32974644bb6d63b5582f3a268d160301e7631
Divya <dsrao0712@gmail.com>
2021-09-24 20:46:20 +0530

    forms.py

--- app/transactions/forms.py
+++ app/transactions/forms.py
@@ -1,1 +1,10 @@
- 
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.fields.core import IntegerField
+from wtforms.validators import (DataRequired, Email, EqualTo, Length,
+                                NumberRange, ValidationError)
+
+class SearchForm(FlaskForm):
+    name = StringField('Book Name' , DataRequired)
+    search = SubmitField('Search')
+    
--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -3,3 +3,6 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 bp = Blueprint('transactions' , __name__)
 
+@bp.route("/book_issue" , methods = ['GET' , 'POST'])
+def book_issue():
+    return render_template("transactions/book_issue.html")
