d032e3bea3641c82f4f93c4f17ab08d169657553
Divya <dsrao0712@gmail.com>
2021-09-25 17:06:03 +0530

    Transactions module - Tables

--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -1,13 +1,18 @@
-
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from app import db
 from app.transactions.forms import SearchForm
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from app.books.models import Books
 
 bp = Blueprint('transactions' , __name__)
 
 @bp.route("/book_issue", methods=['GET', 'POST'])
 def book_issue():
-    
     search_form = SearchForm()
+    books = Books.query
     if search_form.validate_on_submit():
         print("Validating Search ...")
-    return render_template('transactions/book_issue.html', title ='Issue Book', search_form = search_form)
+        data = search_form.data
+        books = books.filter(Books.title.like('%' + data['name'] + '%')) 
+        
+        
+    return render_template('transactions/book_issue.html', title ='Issue Book', search_form = search_form , books = books)
