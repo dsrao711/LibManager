a36c8401492c58948b953f078d93b0aa20e43292
Divya <dsrao0712@gmail.com>
2021-09-29 17:38:22 +0530

    waste comm

--- app/transactions/models.py
+++ app/transactions/models.py
@@ -4,7 +4,7 @@ class Transactions(db.Model):
 
     transaction_id = db.Column(db.Integer , primary_key = True)
     book_name = db.Column(db.String , db.ForeignKey('books.title') )
-    smember_name = db.Column(db.String , db.ForeignKey('members.member_id'))
+    member_name = db.Column(db.String , db.ForeignKey('members.member_id'))
     date = db.Column(db.Date )
     rent_period = db.Column(db.Integer)
     fine = db.Column(db.Integer)
--- app/transactions/routes.py
+++ app/transactions/routes.py
@@ -8,7 +8,7 @@ from app.transactions.models import Transactions
 bp = Blueprint('transactions' , __name__)
 
 @bp.route("/book_issue", methods=['GET', 'POST'])
-def book_issue():
+def book_search():
     search_form = SearchForm()
     issue_form = IssueForm()
     books = Books.query
@@ -18,19 +18,36 @@ def book_issue():
         data = search_form.data
         print(data)
         books = books.filter(Books.title.like('%' + data['name'] + '%')) 
-        
+    
     if issue_form.validate_on_submit():
+        print("Validating Issue Form ...")
         
-        print("Validating Issue Form")
-        issue_form_data = issue_form.data
-        print()
-        print(issue_form_data)
-        member = members.query.filter_by (name = issue_form_data['m_name'])
+        transaction_data = issue_form.data
+        del transaction_data['csrf_token']
+        del transaction_data['submit']
+        print(transaction_data)
         
+        member = members.query.filter_by(name = transaction_data['m_name'])
         if member :
-            print('Member found')
+            print("Member Found")
+            
+            # transaction_complete = Transactions(
+            #     book_name = transaction_data['b_name'] , 
+            #     member_name = transaction_data['m_name'] , 
+            #     date = transaction_data['issue_date'] , 
+            #     rent_period = transaction_data['issue_date'] , 
+            #     fine = transaction_data['fine']
+            # )
+            # db.session.add(transaction_complete)
+            # db.session.commit()      
+             
+            # transaction_complete = Transactions(**transaction_data)
+            # db.session.add(transaction_complete)
+            # db.session.commit()
+            
         else:
-            print('Member not found. Please register the new member')
+            print("Member not found") 
             
     return render_template('transactions/book_issue.html', title ='Issue Book', search_form = search_form , books = books , issue_form = issue_form)
+
 
